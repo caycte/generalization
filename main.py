ff3eac48034d88735ac2be3a5299dd5717461ebc
Christine Chun <christinechun@MacBook-Pro-de-Christine.local>
2024-01-23 18:18:16 +0100

    Add option to gpu on Mac + ignore dataset

--- main.py
+++ main.py
@@ -116,9 +116,27 @@ def main():
                         help='momentum (default: 0.9)')
     args = parser.parse_args()
 
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
-    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+    isMac = True
+
+    if isMac:
+        print(f"PyTorch version: {torch.__version__}")
+
+        # Check PyTorch has access to MPS (Metal Performance Shader, Apple's GPU architecture)
+        print(f"Is MPS (Metal Performance Shader) built? {torch.backends.mps.is_built()}")
+        print(f"Is MPS available? {torch.backends.mps.is_available()}")
+
+        # Set the device      
+        device = "mps" if torch.backends.mps.is_available() else "cpu"
+    else:
+        device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+    print(f"Using device: {device}")
+
+    # use_cuda = not args.no_cuda and torch.cuda.is_available()
+    # device = torch.device("cuda" if use_cuda else "cpu")
+    # kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     nchannels, nclasses, img_dim,  = 3, 10, 32
     if args.dataset == 'MNIST': nchannels = 1
     if args.dataset == 'CIFAR100': nclasses = 100
